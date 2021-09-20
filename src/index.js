d48b7e6d96ba8c98ee0518e548ef9d703dc3afc2
Snaszy <scottnasman@gmail.com>
2021-09-20 14:15:16 -0600

    Should have a dropdown and mobile menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import './style.css';
 
-const menu = document.querySelector('.dropdown-menu');
+document.addEventListener('click', (e) => {
+    const hamburgerMenu = e.target.matches('.bar');
+    if (hamburgerMenu) {
+        document.querySelectorAll('.dropdown-menu').forEach(dropdown => {
+            dropdown.classList.toggle('active');
+        })
+    }
 
-const button = document.querySelector('.dropdown-button');
-button.addEventListener('click', () => {
+    const dropdownButton = e.target.matches('.dropdown-button');
+    if (!dropdownButton && e.target.closest('.dropdown-menu') != null) return;
+
+    let currentDropdown;
+    if (dropdownButton) {
+        currentDropdown = e.target.closest('.dropdown-menu');
+        currentDropdown.classList.toggle('show');
+    }
+
+    document.querySelectorAll('.dropdown-menu.show').forEach(dropdown => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('show');
+    })
+})
+
+/* const buttons = document.querySelectorAll('.dropdown-button');
+buttons.forEach(function(button) {
+    button.addEventListener('click', (e) => {
+        const contents = document.querySelector('.dropdown-content');
+            contents.classList.toggle('show');
+            console.log(contents)
+            console.log(button)
+    })
+}) */
+
+/* document.addEventListener('click', (e) => {
     const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.classList.toggle('show');
-});
+    const button = document.querySelector('.dropdown-button');
+    if (e.target === button) {
+        dropdownContent.classList.toggle('show');
+    } else if (dropdownContent.classList.contains('show')) {
+        dropdownContent.classList.toggle('show');
+    } else {
+        return; 
+    };
+}) */
